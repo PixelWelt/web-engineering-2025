ec8f0eb525860d523e1c8b5d0d1b749476d1d00a
Soeren Herrmann <33161332+PixelWelt@users.noreply.github.com>
2025-04-26 21:27:59 +0200

    finished book browser assignment

--- demos/02/simple-page-with-references/index.js
+++
@@ -1,9 +0,0 @@
-function generateText(event) {
-  const brElement = document.createElement("br");
-  document.body.appendChild(brElement);
-  const pElement = document.createElement("p");
-  pElement.innerText = "Added by JavaScript";
-  document.body.appendChild(pElement);
-}
-
-document.addEventListener("DOMContentLoaded", generateText);
--- demos/03/iframes-2/index.js
+++
@@ -1,5 +0,0 @@
-function previewHTML(event) {
-  htmlEditor = document.getElementById("htmlEditor");
-  previewFrame = document.getElementById("previewFrame");
-  previewFrame.srcdoc = htmlEditor.value;
-}
